1605175e57927be830c557fa2bf782c476bb2b61
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-05-31 18:21:24 +0900

    gaps between pictures in carousel

--- pages/shopByRoom.tsx
+++ pages/shopByRoom.tsx
@@ -1,41 +1,43 @@
-import React, { useEffect } from 'react';
-import Flickity from 'react-flickity-component'
-import "../styles/flickity.css";
+import React from 'react';
+import Flickity from 'react-flickity-component';
+import '../styles/flickity.css';
 
 
-const ShopByRoomPage: React.FC = () => {
-
-
+const ShopByRoomPage = () => {
   const flickityOptions = {
-    initialIndex: 2
-}
+    initialIndex: 2,
+    wrapAround: true,
+    cellSpacing: 10, // Set the desired spacing between images
+  };
+
   return (
     <>
       <h1>Shop by Room</h1>
 
       <Flickity
-      className={'carousel'} // default ''
-      elementType={'div'} // default 'div'
-      options={flickityOptions} // takes flickity options {}
-      disableImagesLoaded={false} // default false
-      reloadOnUpdate // default false
-      static // default false
-    >
-
-    
-    <a href="#"><img src="https://place-puppy.com/500x300"/></a>
-    
-    <a href="#"><img src="https://place-puppy.com/500x300"/></a>
-    
-    <a href="#"><img src="https://place-puppy.com/500x300"/></a>
-    
-    <a href="#"><img src="https://place-puppy.com/500x300"/></a>
-   
-    <a href="#"><img src="https://place-puppy.com/500x300"/></a>
-
-      
-
-    </Flickity>
+        className="carousel"
+        elementType="div"
+        options={flickityOptions}
+        disableImagesLoaded={false}
+        reloadOnUpdate
+        static
+      >
+        <a href="#" className="carousel-cell">
+          <img src="https://place-puppy.com/500x300" alt="Puppy 1" />
+        </a>
+        <a href="#" className="carousel-cell">
+          <img src="https://place-puppy.com/500x300" alt="Puppy 2" />
+        </a>
+        <a href="#" className="carousel-cell">
+          <img src="https://place-puppy.com/500x300" alt="Puppy 3" />
+        </a>
+        <a href="#" className="carousel-cell">
+          <img src="https://place-puppy.com/500x300" alt="Puppy 4" />
+        </a>
+        <a href="#" className="carousel-cell">
+          <img src="https://place-puppy.com/500x300" alt="Puppy 5" />
+        </a>
+      </Flickity>
     </>
   );
 };
