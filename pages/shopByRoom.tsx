e1acc588f521af3cd0bb7144463d08ab03738af1
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-05-31 17:54:49 +0900

    created carousel for shopByRoom page

--- pages/shopByRoom.tsx
+++ pages/shopByRoom.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Flickity from 'react-flickity-component'
+import "../styles/flickity.css";
 
 
-export const ShopByRoom = () => {
-  return (
-    "falwjeflef"
-  )
+const ShopByRoomPage: React.FC = () => {
+
+
+  const flickityOptions = {
+    initialIndex: 2
 }
+  return (
+    <>
+      <h1>Shop by Room</h1>
+
+      <Flickity
+      className={'carousel'} // default ''
+      elementType={'div'} // default 'div'
+      options={flickityOptions} // takes flickity options {}
+      disableImagesLoaded={false} // default false
+      reloadOnUpdate // default false
+      static // default false
+    >
+
+    
+    <a href="#"><img src="https://place-puppy.com/500x300"/></a>
+    
+    <a href="#"><img src="https://place-puppy.com/500x300"/></a>
+    
+    <a href="#"><img src="https://place-puppy.com/500x300"/></a>
+    
+    <a href="#"><img src="https://place-puppy.com/500x300"/></a>
+   
+    <a href="#"><img src="https://place-puppy.com/500x300"/></a>
+
+      
+
+    </Flickity>
+    </>
+  );
+};
+
+export default ShopByRoomPage;
