4e233c8caeb2a2469267e221cf9ccac928f52c4b
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-06-06 22:08:12 +0900

    made edits to services page

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 interface buttonProps {
   path: string
+  inquire?:string
 }
 
 
-const ShopTheCollectionButton = ( { path }: buttonProps ) => {
+const ShopTheCollectionButton = ( { path, inquire }: buttonProps ) => {
   return ( 
-    <div className="shopButton tw-m-24 tw-flex tw-justify-center">
-                <a href={path} className="tw-px-9 tw-py-3 tw-border-solid tw-border-2 tw-border-black tw-rounded-md">SHOP THE COLLECTION</a>
-            </div>
+    <div className={inquire ? "shopButton tw-m-24 tw-flex tw-justify-center tw-mt-6" : "shopButton tw-m-24 tw-flex tw-justify-center"}>
+      <a href={path} className="tw-px-9 tw-py-3 tw-border-solid tw-border-2 tw-border-black tw-rounded-md">{inquire ? inquire : 'SHOP THE COLLECTION'}</a>
+    </div>
   )
 }
 
--- pages/services.tsx
+++ pages/services.tsx
@@ -2,14 +2,33 @@ import React from 'react'
 import Header from '@/components/header'
 import { MainPic } from '@/components/MainPic';
 import Footer from '@/components/Footer';
+import ShopTheCollectionButton from '@/components/Button';
 
 const Services = () => {
   return (
 
     <>
-     
       <Header />
       <MainPic url='img/mainPage.jpg' text={true}/>
+      <div>
+        <p>A modern alternative to the custom workroom. Realize your design vision with bespoke customizations to our collection—each piece tailor-made for your project.</p>
+        <ShopTheCollectionButton path="#" inquire='INQUIRE'/>
+      </div>
+      <div className="tw-bg-[rgb(250,249,247)]">
+        <div className="texts">
+          <h6>BY THE INCH CUSTOMIZATION</h6>
+          <p>Design to the inch with custom widths or extended depths on most pieces, or define cushion configuration to your specification.</p>
+        </div>
+        <div className="texts">
+          <h6>CUSTOMER`S OWN MATERIAL</h6>
+          <p>Upholster with your choice of fabric or leather, or design with mixed materials for the seat, back, or frame for a one-of-a-kind piece.</p>
+        </div>
+        <div className="texts">
+          <h6>TAILORED ADDITIONS</h6>
+          <p>Complement your piece with tailor-made finishing touches—from additional throw pillows to fabric by the yard.</p>
+        </div>
+
+      </div>
       <Footer />
     </>
     
