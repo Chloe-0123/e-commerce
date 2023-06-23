dabb25414baf4fa80bfc5eee3e8830feab3a38e0
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-06-23 21:54:29 +0900

    added signin function

--- pages/api/user/getUser.tsx
+++ pages/api/user/getUser.tsx
@@ -1,22 +1,50 @@
 import connection from '../../../config/db';
-import { useSession } from 'next-auth/react';
 
 export default function handler(req: any, res: any) {
+  const userEmail = req.query.email;
+  console.log('userEmail!!:', userEmail);
 
-  
   try {
     connection.connect((err) => {
       if (err) {
         console.error('Error connecting to MySQL:', err);
         res.status(500).json({ error: 'Error connecting to MySQL' });
       } else {
-        connection.query("SELECT * FROM user WHERE email = 'chloehkim99@gmail.com'", (error, results) => {
+        connection.query(`SELECT * FROM user WHERE email = "${userEmail}"`, (error, results) => {
           if (error) {
             console.error('Error fetching users:', error);
             res.status(500).json({ error: 'Error fetching users' });
           } else {
-            console.log('Fetched users:', results);
-            res.status(200).json(results);
+            if (results.length === 0) {
+              // No user found, insert a new user into the database
+              connection.query(
+                `INSERT INTO user (email) VALUES ("${userEmail}")`,
+                (insertError, insertResults) => {
+                  if (insertError) {
+                    console.error('Error inserting new user:', insertError);
+                    res.status(500).json({ error: 'Error inserting new user' });
+                  } else {
+                    console.log('New user inserted:', insertResults);
+                    // Now you can fetch the user again to get the inserted data
+                    connection.query(
+                      `SELECT * FROM user WHERE email = "${userEmail}"`,
+                      (fetchError, fetchResults) => {
+                        if (fetchError) {
+                          console.error('Error fetching new user:', fetchError);
+                          res.status(500).json({ error: 'Error fetching new user' });
+                        } else {
+                          console.log('Fetched new user:', fetchResults);
+                          res.status(200).json(fetchResults);
+                        }
+                      }
+                    );
+                  }
+                }
+              );
+            } else {
+              console.log('Fetched users:', results);
+              res.status(200).json(results);
+            }
           }
         });
       }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -7,20 +7,31 @@ import { useSession, signOut, signIn } from 'next-auth/react';
 const Profile = () => {
 
   const {data:session} = useSession()
-
+  console.log('profile session', session)
   if (session) {
     console.log('logged in!')
   }
   const [setting, handleSetting] = useState(true);
   const [orders, handleOrders] = useState(false);
   const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('../api/user/getUser')
-      .then((response) => response.json())
-      .then((data) => setUserData(data))
-      .catch((error) => console.error('Error fetching user data:', error));
-  }, []);
+    if (session && session.user && session.user.email) {
+      const userEmail = session.user.email;
+
+      fetch(`../api/user/getUser?email=${encodeURIComponent(userEmail)}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setUserData(data);
+          setLoading(false); // Set loading to false once data is fetched
+        })
+        .catch((error) => {
+          console.error('Error fetching user data:', error);
+          setLoading(false); // Set loading to false if there's an error
+        });
+    }
+  }, [session]); 
 
   
   function handleOrdersClick() {
@@ -44,40 +55,48 @@ const Profile = () => {
   return (
     <>
       <Header />
-      {session ? <div className="tw-flex" style={{ height: 'calc(100vh - 106px)' }}>
-        <div className="righttab tw-w-[20%] tw-bg-slate-200 tw-h-full ">
-          <ul>
-            <li>
-              <a href="#" onClick={handleSettingsClick}>
-                Account Settings
-              </a>
-            </li>
-            <li>
-              <a href="#" onClick={handleOrdersClick}>
-                Orders
-              </a>
-            </li>
-          </ul>
+      {session ? (
+        <div className="tw-flex" style={{ height: 'calc(100vh - 106px)' }}>
+          <div className="righttab tw-w-[20%] tw-bg-slate-200 tw-h-full">
+            <ul>
+              <li>
+                <a href="#" onClick={handleSettingsClick}>
+                  Account Settings
+                </a>
+              </li>
+              <li>
+                <a href="#" onClick={handleOrdersClick}>
+                  Orders
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="leftpage tw-w-[80%] tw-h-full">
+            {setting && !loading ? ( // Check for the loading state here
+              <Settings
+                email={`${userData[0].email}`}
+                name="blah"
+                streetName={`${userData[0].streetName}`}
+                streetNumber={`${userData[0].streetNumber}`}
+                phoneNumber={`${userData[0].phoneNumber}`}
+                city={`${userData[0].city}`}
+                state={`${userData[0].state}`}
+                ZIP={`${userData[0].ZIP}`}
+                addressAvailable={userData[0].streetName ? true : false}
+              />
+            ) : (
+              <Orders />
+            )}
+          </div>
         </div>
-        <div className="leftpage tw-w-[80%] tw-h-full">
-          {setting && userData ? (
-            <Settings
-              email={`${userData[0].email}`}
-              name="blah"
-              streetName={`${userData[0].streetName}`}
-              streetNumber={`${userData[0].streetNumber}`}
-              phoneNumber={`${userData[0].phoneNumber}`}
-              city={`${userData[0].city}`}
-              state={`${userData[0].state}`}
-              ZIP={`${userData[0].ZIP}`}
-            />
-          ) : (
-            <Orders />
-          )}
+      ) : (
+        <div>
+          <p>You are not signed in.</p> <button onClick={() => signIn()}>Sign In</button>
         </div>
-      </div> : <div><p>You are not signed in.</p> <button onClick={() => signIn()}>Sign In</button></div>}
+      )}
     </>
   );
+
 };
 
 export default Profile;
