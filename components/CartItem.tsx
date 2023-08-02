26ba335c4e8e1585231bc36ce6c68a44e8803c2f
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-08-02 22:30:20 +0900

    created cart page

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,51 +1,7 @@
 import React from 'react'
 
-
-export const Orders = () => {
+const CartItem = () => {
   return (
-    <>
-    
-    <div className="orderspage tw-p-16 ">
-        <h2 className='tw-text-[2rem] tw-mb-8'>Orders</h2>
-        <div className="overflow-x-auto">
-  <table className="tw-table tw-w-full">
-    {/* head */}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Order Number</th>
-        <th>Purchased Items</th>
-        <th>Total</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      {/* row 2 */}
-      <tr>
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      {/* row 3 */}
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-    </div>
-    
-
     <div className="tw-overflow-x-auto">
   <table className="tw-table tw-w-full">
     {/* head */}
@@ -104,8 +60,7 @@ export const Orders = () => {
     
   </table>
 </div>
-
-
-    </>
   )
 }
+
+export default CartItem
---
+++ pages/cart.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CartItem from '@/components/CartItem'
+
+const Cart = () => {
+  return (
+    <CartItem />
+  )
+}
+
+export default Cart
