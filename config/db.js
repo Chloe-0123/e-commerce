2a24a2f246e68ec302263e2f2e222f0a69b4f536
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-08-09 23:39:10 +0900

    fixed db error

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ import mysql from 'mysql2';
 const express = require('express');
 
 const connection = mysql.createConnection({
-  host: 'sql9.freemysqlhosting.net',  
-  port: 3306,   // Replace with your MySQL server host
-  user: 'sql9638083',         // Replace with your MySQL username
-  password: 'F1Fay6WY1R',  // Replace with your MySQL password
-  database: 'sql9638083', // Replace with your MySQL schema name
+  host: process.env.DB_HOST,  
+  port: process.env.DB_PORT,   // Replace with your MySQL server host
+  user: process.env.DB_USER,        // Replace with your MySQL username
+  password: process.env.DB_PASSWORD,  // Replace with your MySQL password
+  database: process.env.DB_NAME, // Replace with your MySQL schema name
 });
 
 export default connection;
