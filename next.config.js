bb48914f693249de9f12c625a7447e6afbc3a44b
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-08-09 18:59:45 +0900

    added next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withTM = require('next-transpile-modules')(['mysql2']);
+const path = require('path');
+
+module.exports = withTM({
+  images: {
+    domains: ['place-puppy.com'],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+});
